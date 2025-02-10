5704ad798c882d47bc84673d244cea81e36564c3
Vikas-Padihar <vikas.padihar@mangoitsolutions.in>
2025-02-10 16:21:31 +0530

    Adding login page

--- login.php
+++ login.php
@@ -14,6 +14,7 @@
                 <input type="text" id="username" name="username" required>
             </div>
             
+            
             <div class="form-group">
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password" required>
